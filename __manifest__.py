2aa4e2f2651237d92c06cb6d1eeb9052870ccc29
Muhammad Yusuf Islam <muhammadyusuf7360@gmail.com>
2025-10-27 09:12:16 +0700

    Add initial implementation of Purchase & Sales Connector module

---
+++ __manifest__.py
@@ -0,0 +1,25 @@
+{
+    'name': 'Purchase & Sales Connector',
+    'version': '1.0.0',
+    'category': 'Tools',
+    'summary': 'Connect Purchase and Sales Modules',
+    'description': """
+        Purchase & Sales Connector for Odoo
+    """,
+    'author': 'Yusuf',
+    'website': 'https://github.com/Yusufislam-id/custom_yusuf',
+    'license': 'LGPL-3',
+    'depends': ['sale',
+        'purchase',],
+    'assets': {
+        'web.assets_backend': [
+            
+        ],
+    },
+    'data': [
+        
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
